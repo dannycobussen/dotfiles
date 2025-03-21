edd76e3d9d11d57fa8acec548b34ae0b8e4ed25e
danny cobussen <dcobussen@icloud.com>
2025-03-21 16:30:27 +0100

    Use terafox theme for both

--- nvim/dot-config/nvim/lua/configs/colorscheme.lua
+++ nvim/dot-config/nvim/lua/configs/colorscheme.lua
@@ -1,2 +1,3 @@
 -- vim.cmd([[colorscheme base16-gruvbox-dark-soft]])
-vim.cmd.colorscheme("base16-gruvbox-material-dark-medium")
+-- vim.cmd.colorscheme("base16-gruvbox-material-dark-medium")
+vim.cmd.colorscheme("terafox")
--- nvim/dot-config/nvim/lua/plugins/colorscheme.lua
+++ nvim/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
   -- "tinted-theming/base16-vim",
-  "tinted-theming/tinted-vim",
+  -- "tinted-theming/tinted-vim",
+  "EdenEast/nightfox.nvim",
   config = function()
     require("configs.colorscheme")
   end,
