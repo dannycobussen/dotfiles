73146ab3bed6259fee90ff72c8221d745b314b8e
danny cobussen <danny@dotpixel.nl>
2025-03-24 08:53:34 +0100

    Add copilot plugin

--- nvim/dot-config/nvim/lua/configs/autocomplete.lua
+++ nvim/dot-config/nvim/lua/configs/autocomplete.lua
@@ -28,6 +28,7 @@ cmp.setup({
     end,
   },
   sources = cmp.config.sources({
+    { name = "copilot", group_index = 2 },
     { name = "nvim_lsp" },
     { name = "luasnip" },
     { name = "path" },
